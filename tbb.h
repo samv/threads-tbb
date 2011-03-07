de62a53ee1df14a73acbf89b373d15117b8a3ee0
Sam Vilain <sam@vilain.net>
2011-03-07 16:13:27 +1300

    Make the perl_tbb_init class actually sub-class tbb::task_scheduler_init
    Which, somewhat unexpectedly, just works!  :)  Now to test it's _really_
    working...

--- tbb.h
+++ tbb.h
@@ -1,9 +1,9 @@
 #ifndef _perl_tbb_h_
 #define _perl_tbb_h_
 
-//#include "tbb/task_scheduler_init.h"
+#include "tbb/task_scheduler_init.h"
 
-class perl_tbb_init { //: public tbb::task_scheduler_init {
+class perl_tbb_init : public tbb::task_scheduler_init {
 public:
   perl_tbb_init() {};
   ~perl_tbb_init() {}
