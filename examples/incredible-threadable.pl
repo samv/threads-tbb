1b2e473645a65dc4b1b8bbb2d30e7e82528ec338
Sam Vilain <sam@vilain.net>
2011-04-01 11:48:29 +1300

    Add instrumenting and results to incredible-threadable.pl example
    Some results and discussion.
    
    zcat /usr/share/doc/libx11-dev/libX11.txt.gz | \
       LD_PRELOAD=libtbbmalloc_proxy.so.2 time perl \
          -Mblib examples/incredible-threadable.pl

--- examples/incredible-threadable.pl
+++ examples/incredible-threadable.pl
@@ -1,3 +1,38 @@
+# this example is to verify that the example on the man page works.
+# It's been modified mostly to print statistics and timing
+# information.
+#
+# The problem is just prepending a string with "Ex-", which is mostly
+# serving here as an example of the benefits of the lazy deep copying
+# approach: it generally can only help performance.
+#
+# For example, with threads => 1 and a 1.5MB, 46k line input:
+#   loading input: 134ms
+#   processing: 330ms
+#   Workers processed: 0% :  ()
+#   Master processed: 100% : 46332
+#   Total processed: 46332
+#
+# Setting threads => 2 on a 2-way system shows:
+#
+#   loading input: 134ms
+#   processing: 274ms
+#   Workers processed: 32.8% : 15203 (1 15203)
+#   Master processed: 67.2% : 31129
+#   Total processed: 46332
+#
+# Of course you could argue that the operation is not finished at the
+# instrumented points; the lazy deep copying out of the results is
+# still yet to happen.  But still, the total wallclock time showed a
+# slight decrease with threads => 2, from ~0.74s to ~0.67s - showing
+# that even though we started two interpreters, and even though the
+# program is completely trivial, some performance improvement can be
+# seen at the end of it.  Of course the main thread here was able to
+# process data at approximately 2-3 times the speed of the workers.
+# For this problem, simply copying the data in and out is the major
+# overhead, and so it helps that the master thread can proceed at
+# "full steam" for avoiding the overheads exceeding the return.
+
  package Incredible::Threadable;
  use threads::tbb;
 
@@ -16,11 +51,13 @@
 
      # Initialize the TBB library, and set a specification of required
      # modules and/or library paths for worker threads.
-     my $tbb = threads::tbb->new( requires => [ $0 ] );
+     my $tbb = threads::tbb->new(
+	     #threads => 1,
+	     requires => [ $0 ] );
 
      my $min = 0;
      my $max = scalar(@{ $self->{input} });
-     my $range = threads::tbb::blocked_int->new( $min, $max, 1);
+     my $range = threads::tbb::blocked_int->new( $min, $max, 1 );
 
      my $body = $tbb->for_int_method( $self, "my_callback" );
 
@@ -34,9 +71,9 @@
      for my $idx ($int_range->begin .. $int_range->end-1) {
          my $item = $self->{input}->[$idx];
 
-         my $transmuted = $item->transmogrify;
+         my $transmuted = $item->transmogrify($threads::tbb::worker);
 
-         $self->{output}->[$idx] = [$transmuted, $threads::tbb::worker||0];
+         $self->{output}->[$idx] = $transmuted;
      }
  }
 
@@ -45,19 +82,38 @@
  package Item;
  sub transmogrify {
      my $self = shift;
-     "Ex-$self->{id}";
+     my $worker = shift;
+     "Ex-$self->{id} ".($worker?"worker $worker":"master");;
  }
 
  package main;
  use feature 'say';
 
+ use Scriptalicious;
+ use List::Util qw(sum);
+
  unless ($threads::tbb::worker) {  # single script uses can use this
+     start_timer();
      my $parallel_transmogrificator = Incredible::Threadable->new(
          map { chomp; bless { id => $_ }, "Item" } <>
      );
-
+     say "loading input: ".show_delta;
      $parallel_transmogrificator->parallel_transmogrify();
+     say "processing: ".show_delta;
      my $x = $parallel_transmogrificator;
-     say "Turned to '$_->[0]' from ".($_->[1]?"worker $_->[1]":"master thread")
-	     for $parallel_transmogrificator->results;
+     my %workers;
+     my ($master, $total);
+     for ($parallel_transmogrificator->results) {
+	     $total++;
+	     if ( m{worker (\d+)$} ) {
+		     $workers{$1}++;
+	     }
+	     else {
+		     $master++;
+	     }
+     }
+     my $workers_sum = sum values %workers;
+     say "Workers processed: ".sprintf("%.1f", ($workers_sum/$total*100))."% : $workers_sum (@{[%workers]})";
+     say "Master processed: ".sprintf("%.1f", ($master/$total*100))."% : $master";
+     say "Total processed: $total";
  }
