1e9e0a62f8b12dfd6a8d3bb0b9451c2ae41a3199
Sam Vilain <sam@vilain.net>
2011-06-18 12:11:07 -0400

    Remove some unnecessary STDERR output from a test script

--- t/StaticCB.pm
+++ t/StaticCB.pm
@@ -8,10 +8,8 @@ sub myhandler {
 	my $range = shift;
 	my $array = shift;
 
-	print STDERR "# range: [".$range->begin.",".$range->end.")\n";
 	for ( my $i = $range->begin; $i < $range->end; $i++ ) {
 		my $item = $array->FETCH($i);
-		print STDERR "Got: ".Dumper($item);
 		sleep 0.1;
 	}
 }
